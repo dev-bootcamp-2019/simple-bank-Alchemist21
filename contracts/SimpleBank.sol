e9242c7b97cb30711c1b16894bacdafb0cd8c200
Louell Sala <louell_sala@yahoo.com>
2018-12-01 18:13:44 -0500

    Version5

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -58,7 +58,7 @@ contract SimpleBank {
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
-        require(msg.sender != owner);
+        require(msg.sender != owner,"sender cannot be owner");
         enrolled[msg.sender] = true;
         balances[msg.sender] = 0;
         emit LogEnrolled(msg.sender);
